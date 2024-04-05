f34a7ead940ce8cf9beeb8842246f96e132a9a7d
vinaykanna <vinaykanna367@gmail.com>
2024-04-05 22:59:36 +0530

    limit function invocations

---
+++ lmt_fn_invcns.ts
@@ -0,0 +1,21 @@
+function limit(fn: (...args: any[]) => any, count: number) {
+  let initialCount = 0;
+
+  return function (...args: any[]) {
+    if (initialCount < count) {
+      initialCount++;
+      return fn(...args);
+    }
+  };
+}
+
+function myFun() {
+  console.log("I am being invoked");
+}
+
+const limitedFn = limit(myFun, 2);
+
+limitedFn();
+limitedFn();
+limitedFn(); // will not be invoked
+limitedFn(); // will not be invoked
